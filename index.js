88bed6a66e3f3a74f443668918bc8c1ca52a7da7
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-08-22 22:08:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const bot = require(__dirname + '/lib/amd')
+const { VERSION } = require(__dirname + '/config')
+
+const start = async () => {
+    Debug.info(`Starting Asta ${VERSION}`)
+  try {
+    await bot.init()
+    //bot.logger.info('⏳ Database syncing!')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
+
+if (req.headers.get('Authorization') !== `Bearer ${global.CRON_SECRET}`) {
+  return res.status(401).end('Unauthorized');
+}
